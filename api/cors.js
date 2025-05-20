d532abb33814da5beb19ed7651749c5d6704438e
_ckya_ <108811409+CK-YA@users.noreply.github.com>
2025-05-20 21:49:50 +0530

    Update cors.js

--- api/cors.js
+++ api/cors.js
@@ -1,17 +1,18 @@
 export default async function handler(req, res) {
   const { url } = req.query;
-  if (!url) return res.status(400).send("Missing `url` query");
+  if (!url) {
+    return res.status(400).send("Missing `url` query");
+  }
 
   try {
     const response = await fetch(url);
     const contentType = response.headers.get("content-type");
-
     const buffer = await response.arrayBuffer();
+
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Content-Type", contentType || "application/octet-stream");
     res.send(Buffer.from(buffer));
   } catch (err) {
-    res.status(500).send("Failed to fetch the URL");
+    res.status(500).send("Failed to fetch content");
   }
 }
-
